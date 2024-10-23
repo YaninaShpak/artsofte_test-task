e53f2ce3f1fa48e212280ce48bbdd47703ef2134
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-10-23 11:43:24 +0300

    implement to close modal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 window.addEventListener('DOMContentLoaded', ()=> {
-    console.log('App init');
+  const modal = document.querySelector('.modal'),
+        closeBtn = modal.querySelector('.close-button'),
+        backBtn = modal.querySelector('.back-button');
+
+  closeBtn.addEventListener('click', () => {
+    modal.classList.remove('show');
+  });
+
+  backBtn.addEventListener('click', () => {
+    modal.classList.remove('show');
+  });
+
+  document.body.addEventListener("click", (e) => {
+    const isShowModal =
+      e.target.classList.contains("modal") && modal.classList.contains("show");
+  
+    if (isShowModal) modal.classList.remove("show");
+  });
 })
