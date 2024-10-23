32e18de7ff06c2cf3824e601afcd896c933407d3
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-10-23 11:58:04 +0300

    implement to open modal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,28 @@
-window.addEventListener('DOMContentLoaded', ()=> {
-  const modal = document.querySelector('.modal'),
-        closeBtn = modal.querySelector('.close-button'),
-        backBtn = modal.querySelector('.back-button');
+window.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector(".modal"),
+    closeBtn = modal.querySelector(".close-button"),
+    backBtn = modal.querySelector(".back-button");
+  const card = document.querySelector(".card");
 
-  closeBtn.addEventListener('click', () => {
-    modal.classList.remove('show');
+  card.addEventListener("click", (e) => {
+    const isTitle = e.target.classList.contains("card__title");
+    const isPrice = e.target.classList.contains("specs__desc--price");
+
+    if (!isTitle && !isPrice) modal.classList.add("show");
   });
 
-  backBtn.addEventListener('click', () => {
-    modal.classList.remove('show');
+  closeBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
+  });
+
+  backBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
   });
 
   document.body.addEventListener("click", (e) => {
     const isShowModal =
       e.target.classList.contains("modal") && modal.classList.contains("show");
-  
+
     if (isShowModal) modal.classList.remove("show");
   });
-})
+});
