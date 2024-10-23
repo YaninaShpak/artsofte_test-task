3ad7b49a50b67572d62d6ea943055a1302f2158a
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-10-23 19:23:43 +0300

    refactor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const modal = document.querySelector(".modal"),
-    closeBtn = modal.querySelector(".close-button"),
-    backBtn = modal.querySelector(".back-button");
-  const card = document.querySelector(".card");
+    const modal = document.querySelector(".modal"),
+          closeBtn = modal.querySelector(".close-button"),
+          backBtn = modal.querySelector(".back-button");
+    const card = document.querySelector(".card");
 
-  card.addEventListener("click", (e) => {
-    const isTitle = e.target.classList.contains("card__title");
-    const isPrice = e.target.classList.contains("specs__desc--price");
+    card.addEventListener("click", (e) => {
+        const isTitle = e.target.classList.contains("card__title");
+        const isPrice = e.target.classList.contains("specs__desc--price");
 
-    if (!isTitle && !isPrice) modal.classList.add("show");
-  });
+        if (!isTitle && !isPrice) modal.classList.add("show");
+    });
 
-  closeBtn.addEventListener("click", () => {
-    modal.classList.remove("show");
-  });
+    closeBtn.addEventListener("click", () => {
+        modal.classList.remove("show");
+    });
 
-  backBtn.addEventListener("click", () => {
-    modal.classList.remove("show");
-  });
+    backBtn.addEventListener("click", () => {
+        modal.classList.remove("show");
+    });
 
-  document.body.addEventListener("click", (e) => {
-    const isShowModal =
-      e.target.classList.contains("modal") && modal.classList.contains("show");
+    document.body.addEventListener("click", (e) => {
+        const isShowModal =
+            e.target.classList.contains("modal") &&
+            modal.classList.contains("show");
 
-    if (isShowModal) modal.classList.remove("show");
-  });
+        if (isShowModal) modal.classList.remove("show");
+    });
 });
